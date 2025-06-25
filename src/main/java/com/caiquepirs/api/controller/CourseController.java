cea0502be52f5d492964daef1b32efde4d41ab61
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-25 15:53:54 +0100

    Feat: created the CourseController and the first endpoint to register a course in the database

---
+++ src/main/java/com/caiquepirs/api/controller/CourseController.java
@@ -0,0 +1,37 @@
+package com.caiquepirs.api.controller;
+
+import com.caiquepirs.api.DTOs.CourseRequestDTO;
+import com.caiquepirs.api.DTOs.CourseResponseDTO;
+import com.caiquepirs.api.mappers.CourseMapper;
+import com.caiquepirs.api.service.CourseService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseService service;
+    private final CourseMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<CourseResponseDTO> create(@RequestBody @Valid CourseRequestDTO dto){
+        var course = service.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.toDTO(course));
+    }
+
+
+
+
+
+
+
+
+}
